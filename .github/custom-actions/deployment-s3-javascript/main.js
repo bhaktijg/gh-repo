eff93f811a252f07012c630d1a2fdd06a99fabe6
bhaktijg <bhakti.gogate@volkswagen.co.in>
2025-04-17 13:52:25 +0530

    updated code with s3 cli command

--- .github/custom-actions/deployment-s3-javascript/main.js
+++ .github/custom-actions/deployment-s3-javascript/main.js
@@ -1,9 +1,18 @@
 const core = require('@actions/core');
-const github = require('@actions/github')
-const exec = require('@actions/exec')
+//const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 function run() {
-    core.notice("This message is from custom javascript action.")
+    //1. Get action input values.
+    const bucketName = core.getInput('s3-bucket-name', { required: true });
+    const bucketRegion = core.getInput('s3-bucket-region', { required: true });
+    const websiteFolder = core.getInput('website-folder', { required: true });
+
+    //2. Upload website to S3 bucket.
+    const s3Uri = `s3://${bucketName}`;
+    exec.exec(`aws s3 sync ${websiteFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    core.notice("This message is from custom javascript action.");
 
 }
 
